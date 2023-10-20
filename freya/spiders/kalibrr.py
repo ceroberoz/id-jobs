f914264a4dac74cd19083be3b1fadb28913e3354
Perdana Hadi <ceroberoz@gmail.com>
2023-10-20 11:48:38 +0700

    add Kalibrr

---
+++ freya/spiders/kalibrr.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import json
+from datetime import datetime
+
+
+class KalibrrSpiderJson(scrapy.Spider):
+    name = 'kalibrr-json'
+    base_url = 'https://www.kalibrr.com/kjs/job_board/featured_jobs?limit=1000&offset=1&country=Indonesia'
+
+    # Get timestamp in human readable format
+    now = datetime.now()
+    timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    def start_requests(self):
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # GET request, remove Playwright as it cause breaking in JSON parsing
+        yield scrapy.Request(self.base_url, headers=headers)
+
+    def parse(self, response):
+        try:
+            data = json.loads(response.text)
+            jobs = data['jobs']
+
+            for selector in jobs:
+                # sanitise string from selector['name'] to remove "," in results
+                original_job_title = selector['name']
+                self.sanitize_job_title = original_job_title.replace(", ", " - ")
+
+                yield {
+                    'job_title': self.sanitize_job_title,
+                    'job_location': selector['google_location']['address_components']['city'],
+                    'job_department': 'N/A',
+                    'job_url': f"https://www.kalibrr.com/c/{selector['company_info']['code']}/jobs/{selector['id']}",
+                    'first_seen': self.timestamp, # timestamp job added
+
+                    # # Add job metadata
+                    'base_salary': selector['base_salary'], # salary of job
+                    'job_type': selector['tenure'], # type of job, full-time, part-time, intern, remote
+                    'job_level': 'N/A', # level of job, entry, mid, senior
+                    'job_apply_end_date': selector['application_end_date'], # end date of job
+                    'last_seen': '', # timestamp job last seen
+                    'is_active': 'True', # job is still active, True or False
+
+                    # Add company metadata
+                    'company': selector['company_info']['company_name'], # company name
+                    'company_url': f"https://www.kalibrr.com/id-ID/c/{selector['company_info']['code']}/jobs", #company url
+
+                    # Add job board metadata
+                    'job_board': 'Kalibrr', # name of job board
+                    'job_board_url': 'https://www.kalibrr.com/id-ID/home' # url of job board
+                }
+
+        except json.JSONDecodeError as e:
+            # Handle JSON decoding errors
+            self.logger.error(f"Error decoding JSON: {e}")
+            self.logger.debug(f"Response content: {response.text}")
+
+        # Go to next page, if applicable
+            # next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
+            # if next_page_url is not None:
+            #     yield scrapy.Request(response.urljoin(next_page_url))
