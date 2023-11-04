3714af1ca7d26640f8e2bddf8bd793c172253f28
Perdana Hadi <ceroberoz@gmail.com>
2023-11-04 17:17:15 +0700

    update blibli

--- freya/spiders/blibli.py
+++ freya/spiders/blibli.py
@@ -1,9 +1,10 @@
 import scrapy
+import json
 from datetime import datetime
 
 class BlibliSpiderJson(scrapy.Spider):
     name = 'blibli-json'
-    start_urls = ['https://careers.blibli.com/ext/api/job/list']
+    base_url = 'https://careers.blibli.com/ext/api/job/list.json?format=COMPLETE&groupBy=true'
 
     # Get timestamp in human readable format
     now = datetime.now()
@@ -20,36 +21,42 @@ class BlibliSpiderJson(scrapy.Spider):
     def parse(self, response):
         try:
             data = json.loads(response.text)
-            jobs = data['jobs']
+            jobs = data['responseObject']
 
             for selector in jobs:
-                # sanitise string from selector['name'] to remove "," in results
-                original_job_title = selector['name']
-                self.sanitize_job_title = original_job_title.replace(", ", " - ")
 
-                yield {
-                    'job_title': self.sanitize_job_title,
-                    'job_location': selector['google_location']['address_components']['city'],
-                    'job_department': 'N/A',
-                    'job_url': f"https://www.kalibrr.com/c/{selector['company_info']['code']}/jobs/{selector['id']}",
-                    'first_seen': self.timestamp, # timestamp job added
+                for jobs in selector['jobs']:
 
-                    # Add job metadata
-                    'base_salary': selector['base_salary'], # salary of job
-                    'job_type': selector['tenure'], # type of job, full-time, part-time, intern, remote
-                    'job_level': 'N/A', # level of job, entry, mid, senior
-                    'job_apply_end_date': selector['application_end_date'], # end date of job
-                    'last_seen': '', # timestamp job last seen
-                    'is_active': 'True', # job is still active, True or False
+                    # Create URL with format https://careers.blibli.com/job-detail/junior-engineer-officer?job=f1f4505d-27ef-469e-a9d5-de6d6e199aee
+                    url_path_job_title = jobs['title'].lower().replace(' ', '-')
+                    url_path_job_id = jobs['id']
 
-                    # Add company metadata
-                    'company': selector['company_info']['company_name'], # company name
-                    'company_url': f"https://www.kalibrr.com/id-ID/c/{selector['company_info']['code']}/jobs", #company url
+                    # Merge url_path_job_title and url_path_job_id to url format https://carees.blibli.com/job-detail/{url_path_job_title}?job={url_path_job_id}
+                    url = f"https://carees.blibli.com/job-detail/{url_path_job_title}?job={url_path_job_id}"
 
-                    # Add job board metadata
-                    'job_board': 'Kalibrr', # name of job board
-                    'job_board_url': 'https://www.kalibrr.com/id-ID/home' # url of job board
-                }
+                    yield {
+                        'job_title': jobs['title'],
+                        'job_location': jobs['location'],
+                        'job_department': jobs['departmentName'],
+                        'job_url': url ,
+                        'first_seen': self.timestamp, # timestamp job added
+
+                        # Add job metadata
+                        'base_salary': 'N/A', # salary of job
+                        'job_type': jobs['employmentType'], # type of job, full-time, part-time, intern, remote
+                        'job_level': jobs['experience'], # level of job, entry, mid, senior
+                        'job_apply_end_date': 'n?A', # end date of job
+                        'last_seen': '', # timestamp job last seen
+                        'is_active': 'True', # job is still active, True or False
+
+                        # Add company metadata
+                        'company': 'Blibli', # company name
+                        'company_url': 'https://careers.blibli.com/', #company url
+
+                        # Add job board metadata
+                        'job_board': 'N/A', # name of job board
+                        'job_board_url': 'N/A' # url of job board
+                    }
 
         except json.JSONDecodeError as e:
             # Handle JSON decoding errors
